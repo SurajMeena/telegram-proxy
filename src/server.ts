f2f569b52f73db56e5667881e765598d61e75d00
Suraj Meena <surajmeena505@gmail.com>
2024-07-06 00:18:07 +0530

    chore: Update package.json scripts and dependencies

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,19 @@
 // src/server.ts
 
-import express, { Request, Response } from 'express';
+import express, { Request, Response } from "express";
 import { TelegramClient } from "telegram";
 import { StringSession } from "telegram/sessions";
 import { PhoneMigrateError } from "telegram/errors"; // Import the specific error'
-import dotenv from 'dotenv';
-dotenv.config();
+import dotenv from "dotenv";
 
+dotenv.config();
 
 const SESSION = new StringSession("");
 const API_ID = parseInt(process.env.API_ID || "", 10);
 const API_HASH = process.env.API_HASH || "";
 
 const client = new TelegramClient(SESSION, API_ID, API_HASH, {
-  connectionRetries: 5,
+  connectionRetries: 5
 });
 
 const app = express();
@@ -21,16 +21,13 @@ const port = 3000;
 
 app.use(express.json());
 
-app.post('/send-code', async (req: Request, res: Response) => {
+app.post("/send-code", async (req: Request, res: Response) => {
   const { phoneNumber } = req.body;
 
   while (true) {
     try {
       await client.connect();
-      await client.sendCode(
-        { apiId: API_ID, apiHash: API_HASH },
-        phoneNumber
-      );
+      await client.sendCode({ apiId: API_ID, apiHash: API_HASH }, phoneNumber);
       await client.disconnect();
       res.json({ message: "Code Sent" });
       return;
@@ -46,9 +43,8 @@ app.post('/send-code', async (req: Request, res: Response) => {
   }
 });
 
-app.post('/start-client', async (req: Request, res: Response) => {
+app.post("/start-client", async (req: Request, res: Response) => {
   const { phoneNumber, phoneCode, password } = req.body;
-
   while (true) {
     try {
       await client.connect();
@@ -58,7 +54,7 @@ app.post('/start-client', async (req: Request, res: Response) => {
         phoneCode: async () => phoneCode,
         onError: (err) => {
           throw err;
-        },
+        }
       });
 
       const savedSession = client.session.save();
@@ -76,10 +72,10 @@ app.post('/start-client', async (req: Request, res: Response) => {
         await client.connect();
         continue;
       }
-    
+
       console.error("Error starting client: ", error);
       res.status(500).json({
-        error: "Failed to start client",
+        error: "Failed to start client"
       });
       return;
     }
